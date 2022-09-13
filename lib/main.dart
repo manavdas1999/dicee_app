75dbfec97ece93d5d0337f5f8e7b6ad5fef33678
manav <dascorp@gmail.com>
2022-09-13 20:22:20 +0530

    base featured app completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,66 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          backgroundColor: Colors.redAccent,
+          title: const Text("Dicee!"),
+        ),
+        body: DicePage(),
+      ),
+    );
   }
 }
+
+class DicePage extends StatefulWidget {
+  DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dicenum1 = 6;
+  int dicenum2 = 6;
+
+  void getRandomDice(){
+    setState(() {
+      dicenum1 = Random().nextInt(6) + 1;
+      dicenum2 = Random().nextInt(6) + 1;
+    });
+
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    getRandomDice();
+    return Container(
+      child: Center(
+        child: Row(
+          children: [
+            Expanded(
+                child: TextButton(
+                  onPressed: (){
+                    getRandomDice();
+                  },
+                  child: Image(image: AssetImage('images/dice$dicenum1.png'),),
+                ),
+              ),
+            Expanded(
+              child: TextButton(
+                onPressed: (){
+                  getRandomDice();
+                },
+                child: Image(image: AssetImage('images/dice$dicenum2.png'),),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
